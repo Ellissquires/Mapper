24c2c27a2f63c5e8c284457ce5dca246923409df
NevilleKitala <nevillekitala@gmail.com>
2019-11-17 20:06:59 +0000

    Equilized the colour schemes

--- app/src/main/java/com/example/mapper/MainActivity.java
+++ app/src/main/java/com/example/mapper/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visit);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
