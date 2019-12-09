5954d8ec27a740bd3a7df4938d6be183a6b69457
ellis <ellis.squires@gmail.com>
2019-12-09 12:40:55 +0000

    Database no longer wipes on install

--- app/src/main/java/com/example/mapper/services/database/ApplicationDatabase.java
+++ app/src/main/java/com/example/mapper/services/database/ApplicationDatabase.java
@@ -66,9 +66,8 @@ public abstract class ApplicationDatabase extends RoomDatabase {
 
         @Override
         protected Void doInBackground(final Void... params) {
-            mDao.deleteAll();
-            mDao.insert(new Visit("Athens", "Blah", new Date(),0));
-            mDao.insert(new Visit("London", "Blah", new Date(),0));
+            mDao.insert(new Visit("Athens", "Description", new Date(),0));
+            mDao.insert(new Visit("London", "Description", new Date(),0));
 
             return null;
         }
