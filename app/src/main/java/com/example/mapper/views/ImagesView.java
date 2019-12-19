eee259d3483eab9eb99d323faec7169be2221e0c
NevilleKitala <nevillekitala@gmail.com>
2019-12-19 22:11:47 +0000

    Fixed Visit gallery

--- app/src/main/java/com/example/mapper/views/ImagesView.java
+++ app/src/main/java/com/example/mapper/views/ImagesView.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mapper.R;
+import com.example.mapper.services.ImageFetchService;
 
 import java.io.File;
 
@@ -20,11 +21,13 @@ public class ImagesView extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         File element = (File) bundle.get("image");
-
+        String tag  = bundle.getString("tag");
         if(element != null){
             ImageView imageView = (ImageView) findViewById(R.id.image);
             if (element!=null) {
                 Bitmap myBitmap = BitmapFactory.decodeFile(element.getAbsolutePath());
+                if (tag.equals("rotate"))
+                    myBitmap = ImageFetchService.rotateBitmap(myBitmap, 90);
                 imageView.setImageBitmap(myBitmap);
             }
         }
